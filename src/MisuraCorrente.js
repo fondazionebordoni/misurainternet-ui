c34aff1561cdb8918d0a56d05e29b34490ca8104
Jerin George Mathew <jerin.edassery95@gmail.com>
2017-08-10 11:34:23 +0200

    aggiunto componente MisuraCorrente.js

---
+++ src/MisuraCorrente.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pulsante from './Pulsante';
+
+class MisuraCorrente extends React.Component{
+  render(){
+    return (
+      <div>
+        <Pulsante />
+      </div>
+    )
+
+  }
+}
+
+export default MisuraCorrente;
