1f51899bb3e98989f3c383cf465c437f4ca0cd34
Jerin George Mathew <jerin.edassery95@gmail.com>
2017-08-10 10:39:05 +0200

    aggiunto primo componente

---
+++ src/Intestazione.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Intestazione = () =>{
+  return (
+    <div>
+      <h2>MisuraInternet UI</h2>
+        <p>Interfaccia web per il monitoraggio della
+          qualità degli accessi ad Internet da postazione fissa
+          realizzato da AGCOM in collaborazione con la Fondazione
+          Ugo Bordoni ed il supporto dell’Istituto Superiore
+          delle Comunicazioni.
+        </p>
+    </div>
+  )
+}
+
+export default Intestazione;
