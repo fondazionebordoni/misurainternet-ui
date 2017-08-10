e431d8bf7e3f0487e7e4d39c0aea615afb623f80
calderone <darkal95@libero.it>
2017-08-10 12:22:35 +0200

    corretta sintassi

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Intestazione from './Intestazione';
 import MisuraCorrente from './MisuraCorrente';
 
-import MisuraCorrente from './MisuraCorrente';
-
 class App extends Component {
   render() {
     return (
